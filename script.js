d0d605ebfdeb3b68bd3b6fcf61a77367f9ef394b
rruiz88 <roman.ruiz816@gmail.com>
2022-08-11 20:38:55 -0500

    Added some CSS

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ window.addEventListener("load", function(){
         response.json().then(function(json){
             const container = document.getElementById("container");
         //let i =0;
+        json.sort((a,b)=>(a.hoursInSpace - b.hoursInSpace));
            for (let i=0;i<json.length;i++){
             
             container.innerHTML += `
+
+            <h3>Astronaut # ${[i].length+i}</h3>
             <div class="astronaut">
             <div class="bio">
             <h3>${json[i].firstName} ${json[i].lastName}</h3>
             <ul>
             <li>Hours in space: ${json[i].hoursInSpace}</li>
-            <li class='${json[i].active ? "green" : "red"}'>Active: ${json[i].active}</li>
+            <li class='${json[i].active}'>Active: ${json[i].active}</li>
             <li>Skills: ${json[i].skills}</li>
             </ul>
             </div>
             <img class="avatar" src=${json[i].picture}>
             </div>
+            
             `;
-           } 
+            
+           };
+           
         })
      });
 
